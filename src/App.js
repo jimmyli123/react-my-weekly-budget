60f08f0b209c885c8943c51e25555bd773ddedb6
Jimmy Li <lijjimmy@gmail.com>
2023-01-23 19:32:20 -0500

    change certain headings

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function openAddExpenseModal(budgetId) {
   <>
   <Container className="my-4">
     <Stack direction="horizontal" gap="2" className="mb-4">
-      <h1 className="me-auto">Budgets</h1>
+      <h1 className="me-auto">My weekly budget</h1>
       <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>Add Budget</Button>
       <Button variant="outline-primary" onClick={openAddExpenseModal}>Add Expense</Button>
     </Stack>
